ffdc32e9ecb8e696a2becb648e0bcf68da21ee7d
Paul Depew <pauldepew@comcast.net>
2020-04-21 10:36:53 -0700

    css add

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
 'use strict';
 
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('./public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get ('/', renderHome);
+
+app.listen(process.env.PORT || 3000, (request, response) => {
+  console.log('app is up on port: ' + process.env.PORT);
+});
+
+function renderHome(request, response){
+  response.render('pages/index');
+};
